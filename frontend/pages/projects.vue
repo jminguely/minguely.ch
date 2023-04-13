03aa44be3fa2cc59197c4407b1952f991a3b0137
Julien Minguely <jminguely@gmail.com>
2023-04-13 19:00:49 +0200

    Hotfix…

--- frontend/pages/projects.vue
+++ frontend/pages/projects.vue
@@ -1,9 +1,21 @@
 <script setup>
 const route = useRoute();
+
+const { find } = useStrapi();
+
+const response = await find("projects");
+
+const projects = response.data;
 </script>
 
 <template>
   <div>
     <p>Current route: {{ route.path }}</p>
+    <div v-if="projects && projects.length">
+      <h1 class="text-2xl">Projects</h1>
+      <div v-for="project in projects" :key="project.id">
+        <p>Project</p>
+      </div>
+    </div>
   </div>
 </template>
