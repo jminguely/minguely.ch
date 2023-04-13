37e6f88129123066662b7ed1d97ab72e17b66db5
Julien Minguely <jminguely@gmail.com>
2023-04-13 18:57:55 +0200

    Simplify the project page

--- frontend/pages/projects.vue
+++ frontend/pages/projects.vue
@@ -1,22 +1,9 @@
 <script setup>
 const route = useRoute();
-
-const { find } = useStrapi();
-
-const response = await find("projects");
-
-const projects = response.data;
 </script>
 
 <template>
   <div>
     <p>Current route: {{ route.path }}</p>
-    <div v-if="projects && projects.length">
-      <h1 class="text-2xl">Projects</h1>
-      <div v-for="project in projects" :key="project.id">
-        <h2>{{ project.attributes.title }}</h2>
-        <h2>{{ project.attributes.subtitle }}</h2>
-      </div>
-    </div>
   </div>
 </template>
