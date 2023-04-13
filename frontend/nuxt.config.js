005459a4fbde0405beeaf3b2f1ade4f316714da2
Julien Minguely <jminguely@gmail.com>
2023-04-13 20:04:30 +0200

    Revamps the project page with apollo

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,23 +1,25 @@
 import gql from "@rollup/plugin-graphql"
 
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/strapi'
-  ],
-  runtimeConfig: {
-    public: {
-      strapi: {
-        url: 'https://api.minguely.ch'
-      },
-    }
+  modules: ['@nuxtjs/apollo'],
+
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: 'https://api.minguely.ch/graphql'
+      }
+    },
   },
+
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   vite: {
     plugins: [gql()]
   },
--- frontend/pages/projects.vue
+++ frontend/pages/projects.vue
@@ -1,21 +1,42 @@
 <script setup>
-const route = useRoute();
-
-const { find } = useStrapi();
-
-const response = await find("projects");
-
-const projects = response.data;
+const query = gql`
+  query getProjects {
+    projects {
+      data {
+        id
+        attributes {
+          title
+          subtitle
+          description
+          external_url
+          cover {
+            data {
+              id
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+const { data } = await useAsyncQuery(query);
 </script>
 
 <template>
   <div>
-    <p>Current route: {{ route.path }}</p>
-    <div v-if="projects && projects.length">
+    <div v-if="data?.projects?.data?.length">
       <h1 class="text-2xl">Projects</h1>
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in data.projects.data" :key="project.id">
         <h2>{{ project.attributes.title }}</h2>
         <p>{{ project.attributes.subtitle }}</p>
+        <img
+          class="w-96"
+          provider="strapi"
+          :src="`https://api.minguely.ch${project.attributes.cover.data.attributes.url}`"
+        />
       </div>
     </div>
   </div>
