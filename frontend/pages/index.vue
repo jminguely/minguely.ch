0f7caf0d5c004ad05e7139952d6c7d0d928347b2
Julien Minguely <jminguely@gmail.com>
2023-04-13 18:40:08 +0200

    Adds a homepage

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,27 +1,11 @@
-<script setup>
-import query from "~/query/fetchProjects.gql";
-
-const route = useRoute();
-
-const graphql = useStrapiGraphQL();
-
-const { data } = await graphql(query);
-
-const {
-  projects: { data: projects },
-} = data;
-</script>
+<style scoped>
+html {
+  background: grey;
+}
+</style>
 
 <template>
   <div>
     <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <div v-if="projects.length">
-      <h2 class="text-2xl">Projects</h2>
-      <div v-for="project in projects" :key="project.id">
-        <h2>{{ project.attributes.title }}</h2>
-        <h2>{{ project.attributes.subtitle }}</h2>
-      </div>
-    </div>
   </div>
 </template>
---
+++ frontend/pages/projects.vue
@@ -0,0 +1,26 @@
+<script setup>
+import query from "~/query/fetchProjects.gql";
+
+const route = useRoute();
+
+const graphql = useStrapiGraphQL();
+
+const { data } = await graphql(query);
+
+const {
+  projects: { data: projects },
+} = data;
+</script>
+
+<template>
+  <div>
+    <p>Current route: {{ route.path }}</p>
+    <div v-if="projects.length">
+      <h1 class="text-2xl">Projects</h1>
+      <div v-for="project in projects" :key="project.id">
+        <h2>{{ project.attributes.title }}</h2>
+        <h2>{{ project.attributes.subtitle }}</h2>
+      </div>
+    </div>
+  </div>
+</template>
