74c1ae0210fb9f318530282d42fee1be1743b64e
Julien Minguely <jminguely@gmail.com>
2026-06-06 16:47:30 +0200

    updates the cms to add hightlight and concert list

--- frontend/components/ProjectsList.vue
+++ frontend/components/ProjectsList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card-container" v-if="data?.projects">
     <ProjectCard
-      v-for="project in data.projects.data"
+      v-for="project in sortedProjects"
       :key="project.id"
       :project="project"
     />
@@ -18,6 +18,13 @@ const props = defineProps({
 const variables = { category: props.category };
 
 const { data } = await useAsyncQuery(fetchProjects, variables);
+
+const sortedProjects = computed(() => {
+  if (!data.value?.projects?.data) return [];
+  return [...data.value.projects.data].sort((a, b) => {
+    return (b.attributes.highlight ? 1 : 0) - (a.attributes.highlight ? 1 : 0);
+  });
+});
 </script>
 
 <style scoped lang="postcss"></style>
